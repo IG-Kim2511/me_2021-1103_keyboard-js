41ee0a49010cfcf07fb94d9ab7c23d5663c6435f
iikim <iikim2511@gmail.com>
2021-11-03 21:18:29 -0400

    keyboard-num

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 "use strict";
 
 
+// 🍀keyboard - const
+const keyboardBtn  = document.querySelector('.keyboard-btn');
+
+// login
+const keyboardLogin = document.querySelector('.keyboard-login');
+const loginDisplay = document.querySelector('.display-login');
+const loginEnterKey = document.querySelector('.keyboard-login .enter');
+const loginCloseKey = document.querySelector('.keyboard-login .close');
+
+
+
 // 🍀js1004-40. keyboard-login : open
 
-keyboardBtn.addEventListener('click',()=>{
-	keyboardLogin.classList.add('change');	
-	loginDisplay.value = "";
-	keyboardDisplay.value = "";
-});
+// loginDisplay.value = "";
+// keyboardDisplay.value = "";
 
 // 🍀 keyboard-login : close 
 loginCloseKey.addEventListener('click',()=>{
@@ -48,5 +56,55 @@ loginEnterKey.addEventListener('click',()=>{
 	} else {	
 		console.log(`wrong number`)
 		loginDisplay.value = "";
+	}
+});
+
+
+
+//keyboard - event bubbling
+
+keyboardLogin.addEventListener('click',(e)=>{
+	
+	console.log(loginDisplay.value);
+
+	if (e.target.classList.contains('key_1')) {
+		let valu = loginDisplay.value;
+		loginDisplay.value=valu+'1';
+	}
+	if (e.target.classList.contains('key_2')) {		
+		let valu = loginDisplay.value;
+		loginDisplay.value=valu+'2';
+	}
+	if (e.target.classList.contains('key_3')) {		
+		let valu = loginDisplay.value;
+		loginDisplay.value=valu+'3';
+	}
+	if (e.target.classList.contains('key_4')) {
+		let valu = loginDisplay.value;
+		loginDisplay.value=valu+'4';
+	}
+	if (e.target.classList.contains('key_5')) {
+		let valu = loginDisplay.value;
+		loginDisplay.value=valu+'5';
+	}
+	if (e.target.classList.contains('key_6')) {
+		let valu = loginDisplay.value;
+		loginDisplay.value=valu+'6';
+	}
+	if (e.target.classList.contains('key_7')) {
+		let valu = loginDisplay.value;
+		loginDisplay.value=valu+'7';
+	}
+	if (e.target.classList.contains('key_8')) {
+		let valu = loginDisplay.value;
+		loginDisplay.value=valu+'8';
+	}
+	if (e.target.classList.contains('key_9')) {
+		let valu = loginDisplay.value;
+		loginDisplay.value=valu+'9';
+	}
+	if (e.target.classList.contains('key_0')) {		
+		let valu = loginDisplay.value;
+		loginDisplay.value=valu+'0';
 	}
 });
