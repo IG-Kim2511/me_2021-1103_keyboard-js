8034ea91755c0fa7baadb3435d2bef32b2528495
iikim <iikim2511@gmail.com>
2021-11-03 21:23:17 -0400

    fin

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 
 // 🍀keyboard - const
+const title = document.querySelector('.title');
+
+
 const keyboardBtn  = document.querySelector('.keyboard-btn');
 
 // login
@@ -14,49 +17,20 @@ const loginCloseKey = document.querySelector('.keyboard-login .close');
 
 // 🍀js1004-40. keyboard-login : open
 
-// loginDisplay.value = "";
-// keyboardDisplay.value = "";
+
 
 // 🍀 keyboard-login : close 
 loginCloseKey.addEventListener('click',()=>{
-	keyboardLogin.classList.remove('change');
+    loginDisplay.value = "";
 });
 
 // 🍀 keyboard-login : Enter - cashier number & open keyboardContainer
 
 loginEnterKey.addEventListener('click',()=>{
 
-	// cashier_number
-	if ( loginDisplay.value == cashier_number) {		
-		keyboardPassword.classList.add('change');	
-		
-		cashierElem.textContent = `CASHIER ${cashier_number}`;
-
-		
-		worldRight_btnContainer.style.display="grid"
-		// 🍉js1025. 
-		page1DiscountBtn.style.display =`block`; 
-		page1CancelBtn.style.display =`block`; 
-		logoutBtn.style.display=`block`;
-		
-	} 
-
-	//cashier_number20 - hide 'discount'btn, 'Cancel transaction'btn
-	else if ( loginDisplay.value == cashier_number20) {		
-		keyboardPassword.classList.add('change');	
-		
-		cashierElem.textContent = `CASHIER ${cashier_number20}`;
-		
-		worldRight_btnContainer.style.display="grid"
-		// 🍉js1025. "item discount"btn, "cancle transaction" : display:none 	
-		page1DiscountBtn.style.display =`none`; 
-		page1CancelBtn.style.display =`none`; 
-		logoutBtn.style.display=`none`;
-
-	} else {	
-		console.log(`wrong number`)
-		loginDisplay.value = "";
-	}
+    title.innerHTML = loginDisplay.value;
+    
+    loginDisplay.value = "";
 });
 
 
